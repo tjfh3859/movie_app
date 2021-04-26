743a5253ce395768dde2c40e8d50354f61276e5a
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-04-26 16:54:00 +0900

    Planning Movie Component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React from 'react'
-import PropTypes from "prop-types";
 
-function Movie({ year }) {
-  return <h3>{year} 영화</h3>
-}
-
-Movie.propTypes = {
-  year: PropTypes.string.isRequired
-};
-
-const movieYear = [
-  {
-    id: 1,
-    year: "2021"
-  },
-  {
-    id: 2,
-    year: "2020"
-  },
-  {
-    id: 3,
-    year: "2019"
-  },
-  {
-    id:4,
-    year: "2018"
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-]
-
-function renderMovie(whenYear){
-  console.log(whenYear);
-  return <Movie year={whenYear.year} />
-}
-
-function App() {
-  return (
-    <div>
-      {movieYear.map(renderMovie)}
-    </div>
-  );
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
 }
 
 export default App;
