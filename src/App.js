cb08041572fb1b05993edb66bec87748ddce5e2b
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-04-29 14:26:51 +0900

    Add About.js
    console.log(props),

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={ Home } />
       <Route path="/about" component={ About } />
+      <Route path="/movie/:id" component={ Detail } />
     </HashRouter>
   )
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-    return <span>About page</span>
+    return (
+        <div className="about__container">
+            <span>About page</span>
+        </div>
+    );
 }
 
 export default About
