b8fbe9e17860d16843903ecc00b70f9390c52f0b
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-04-29 13:47:39 +0900

    Add Naviation and App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
-
-function App(){
-  return <span>Something</span>
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={ Home } />
+      <Route path="/about" component={ About } />
+    </HashRouter>
+  )
 }
 
 export default App;
